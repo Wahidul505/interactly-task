f221f82d3eada42729c4dbbe1e79295d800a272a
Wahidul30 <wahidul505@gmail.com>
2022-08-09 22:52:10 +0600

    deployed

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
     const navigate = useSelector(state => state.navigate);
 
     return (
-        <div className={`fixed lg:bottom-12 lg:left-12 lg:right-auto bg-[#7D00FE] bg-opacity-75 lg:rounded-xl text-white px-2 py-1 text-start bottom-0 right-0 left-0 flex lg:flex-col items-center justify-center gap-1 ${navigate.form && 'hidden lg:block'}`}>
+        <div className={`fixed lg:bottom-12 lg:left-12 lg:right-auto bg-[#7D00FE] bg-opacity-75 lg:rounded-xl text-white px-2 py-1 text-start bottom-0 right-0 left-0 flex lg:flex-col items-center justify-center gap-1 lg:items-start ${navigate.form && 'hidden lg:block'}`}>
             <p className='font-semibold text-sm'>Lorem:</p>
             <p className='flex gap-1 items-center text-xl font-semibold'><CgSmileMouthOpen className='text-2xl' />ipsum</p>
         </div>
