a2df4040f32d6a121ba59f8ddf7b4924d020311b
Wahidul30 <wahidul505@gmail.com>
2022-08-09 22:58:40 +0600

    fixed elements positioning and deployed finally

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -7,8 +7,8 @@ const Logo = () => {
 
     return (
         <div className={`fixed lg:bottom-12 lg:left-12 lg:right-auto bg-[#7D00FE] bg-opacity-75 lg:rounded-xl text-white px-2 py-1 text-start bottom-0 right-0 left-0 flex lg:flex-col items-center justify-center gap-1 lg:items-start ${navigate.form && 'hidden lg:block'}`}>
-            <p className='font-semibold text-sm'>Lorem:</p>
-            <p className='flex gap-1 items-center text-xl font-semibold'><CgSmileMouthOpen className='text-2xl' />ipsum</p>
+            <p className='font-semibold text-sm'>Powered by:</p>
+            <p className='flex gap-1 items-center text-xl font-semibold'><CgSmileMouthOpen className='text-2xl' />videoask</p>
         </div>
     );
 };
